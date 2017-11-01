4588eaadddfb623ee9bf1521548a2dfd09a71c0e
Marcelo <marcelo2802@gmail.com>
2017-11-01 00:51:53 -0200

    Retirado warnings da aplicação.

--- FastFood.Service/LancheService.cs
+++ FastFood.Service/LancheService.cs
@@ -48,9 +48,12 @@ namespace FastFood.Service
         {
             lanche.Valor = 0;
 
-            foreach (var ingrediente in lanche.IngredienteIds)
+            if (lanche.IngredienteIds != null)
             {
-                lanche.Valor = lanche.Valor + this.repository.RetornaIndedientePorId(ingrediente).Valor;
+                foreach (var ingrediente in lanche.IngredienteIds)
+                {
+                    lanche.Valor = lanche.Valor + this.repository.RetornaIndedientePorId(ingrediente).Valor;
+                }
             }
 
             //Verificar e retorna a promoção muita carne e se caso for uma promocao muita carne
@@ -68,26 +71,29 @@ namespace FastFood.Service
             bool temAlface = false;
             bool temBacon = false;
 
-            foreach (var item in lanche.IngredienteIds)
+            if (lanche.IngredienteIds != null)
             {
-                //Verifica se tem alface
-                if (item == 1)
-                    temAlface = true;
+                foreach (var item in lanche.IngredienteIds)
+                {
+                    //Verifica se tem alface
+                    if (item == 1)
+                        temAlface = true;
 
-                //Verifica se tem bacon
-                if (item == 2)
-                    temBacon = true;
+                    //Verifica se tem bacon
+                    if (item == 2)
+                        temBacon = true;
+                }
+
+                // 10%
+                float percentual = 10.0f / 100.0f;
+
+                //Se tiver alface e nao tiver bacon, e uma lanche ligth e recebe o desconto
+                if (temAlface && !temBacon)
+                    lanche.Valor = lanche.Valor - (percentual * lanche.Valor);
+
+                //Arrendoda valor e mantem duas casas decimais
+                lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
             }
-
-            // 10%
-            float percentual = 10.0f / 100.0f;
-
-            //Se tiver alface e nao tiver bacon, e uma lanche ligth e recebe o desconto
-            if (temAlface && !temBacon)
-                lanche.Valor = lanche.Valor - (percentual * lanche.Valor);
-
-            //Arrendoda valor e mantem duas casas decimais
-            lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
 
             return lanche.Valor;
         }
@@ -96,24 +102,27 @@ namespace FastFood.Service
         {
             int carne = 0;
 
-            float valorCarne = this.repository.RetornaIndedientePorId(3).Valor;
-
-            foreach (var item in lanche.IngredienteIds)
+            if (lanche.IngredienteIds != null)
             {
-                //Verifica se o ingrediente é carne
-                if (item == 3)
-                    carne++;
+                float valorCarne = this.repository.RetornaIndedientePorId(3).Valor;
 
-                //Se já tiver 3 escolhas de carne, e descontado o valor de uma das carnes
-                if (carne == 3)
+                foreach (var item in lanche.IngredienteIds)
                 {
-                    lanche.Valor = lanche.Valor - valorCarne;
-                    carne = 0;  
-                }
-            }
+                    //Verifica se o ingrediente é carne
+                    if (item == 3)
+                        carne++;
 
-            //Arrendoda valor e mantem duas casas decimais
-            lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
+                    //Se já tiver 3 escolhas de carne, e descontado o valor de uma das carnes
+                    if (carne == 3)
+                    {
+                        lanche.Valor = lanche.Valor - valorCarne;
+                        carne = 0;
+                    }
+                }
+
+                //Arrendoda valor e mantem duas casas decimais
+                lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
+            }
 
             return lanche.Valor;
         }
@@ -122,24 +131,27 @@ namespace FastFood.Service
         {
             int queijo = 0;
 
-            float valorQueijo = this.repository.RetornaIndedientePorId(5).Valor;
-
-            foreach (var item in lanche.IngredienteIds)
+            if (lanche.IngredienteIds != null)
             {
-                //Verifica se o ingrediente é queijo
-                if (item == 5)
-                    queijo++;
+                float valorQueijo = this.repository.RetornaIndedientePorId(5).Valor;
 
-                //Se já tiver 3 escolhas de queijo, e descontado o valor um dos queijo
-                if (queijo == 3)
+                foreach (var item in lanche.IngredienteIds)
                 {
-                    lanche.Valor = lanche.Valor - valorQueijo;
-                    queijo = 0;
-                }
-            }
+                    //Verifica se o ingrediente é queijo
+                    if (item == 5)
+                        queijo++;
 
-            //Arrendoda valor e mantem duas casas decimais
-            lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
+                    //Se já tiver 3 escolhas de queijo, e descontado o valor um dos queijo
+                    if (queijo == 3)
+                    {
+                        lanche.Valor = lanche.Valor - valorQueijo;
+                        queijo = 0;
+                    }
+                }
+
+                //Arrendoda valor e mantem duas casas decimais
+                lanche.Valor = float.Parse(Math.Round(lanche.Valor, 2).ToString());
+            }
 
             return lanche.Valor;
         }
